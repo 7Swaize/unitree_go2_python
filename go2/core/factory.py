554c15bf06c077118dc72464ac5ef39360577087
7Swaize <rahejasachit@gmail.com>
2026-08-13 06:20:40 +0000

    Add doc comment for 'create_controller' factory method (#73)

--- go2/core/factory.py
+++ go2/core/factory.py
@@ -12,5 +12,6 @@ def create_controller(hardware_type: Literal[HardwareType.NATIVE], execution_mod
 def create_controller(hardware_type: Literal[HardwareType.VIRTUAL], execution_mode: ExecutionMode = ...) -> VirtualGo2Controller: ...
 
 def create_controller(hardware_type: HardwareType, execution_mode: ExecutionMode = ExecutionMode.BASIC) -> Go2Controller:
+    """Factory method to create a Go2Controller instance."""
     cls = NativeGo2Controller if hardware_type == HardwareType.NATIVE else VirtualGo2Controller
     return cls(execution_mode)
